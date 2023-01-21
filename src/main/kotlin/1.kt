1421c650346c63e14382c1a4a7753569b6360374
apoorva.manjunath5@gmail.com <apoorva.manjunath5@gmail.com>
2023-01-22 00:50:55 +0530

    kotlin!!

--- src/main/kotlin/1.kt
+++ src/main/kotlin/1.kt
@@ -1,12 +1,17 @@
-fun main(){
-    val myArray = arrayOf(4,5,6,7,8,9)
-    var max = myArray[0]
-    for(item in myArray)
-    {
-        if(item>max)
-        {
-            max = item
+// find the sum on n odd and even numbers
+fun main() {
+    println("enter number of digits: ")
+    val n = readLine()?.toInt()!!
+    var result1 = 0
+    var result2 = 0
+    for (i in 0..n) {
+        if (i % 2 == 0) {
+            result1 +=i
+        }
+        else {
+            result2 +=i
         }
     }
-    println(max)
+    println("the sum of the even numbers is: $result1 ")
+    println("the sum of the odd numbers is: $result2 ")
 }
