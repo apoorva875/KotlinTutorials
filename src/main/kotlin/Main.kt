4f8da6b6a504dee012136854ae37a792c3a76dc6
apoorva.manjunath5@gmail.com <apoorva.manjunath5@gmail.com>
2023-01-21 14:55:41 +0530

    kotlin!!

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,8 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
-
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+fun main(){
+    for(i in 1..4){
+        for( i in 1..i){
+            print("*")
+        }
+        println("")
+    }
 }
